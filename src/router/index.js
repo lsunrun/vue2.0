61808efa9af6eccfd09614016e2dc15bfb1bc29f
lxy <lsunrun@163.com>
2019-08-29 14:45:16 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import HelloWorld from '@/components/HelloWorld'
-
+Vue.use(ElementUI)
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+const routes = [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  },
+  {
+    path: '*',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }
+
+]
+
+
+
+export default new Router({ routes })
